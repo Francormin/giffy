158fcd77db849e1e8cdc06a57a2cf2abec1e253a
Franco <francocorniglione5@gmail.com>
2025-08-12 12:52:51 -0300

    refactoring ListOfGifs component

--- src/components/ListOfGifs.js
+++ src/components/ListOfGifs.js
@@ -1,14 +1,48 @@
+import { useEffect, useState } from "react";
+import getGifs from "../services/getGifs";
 import Gif from "./Gif";
 
-const ListOfGifs = ({ gifs }) => {
-  return gifs.map(({ id, title, url }) =>
-    <Gif
-      key={id}
-      id={id}
-      title={title}
-      url={url}
-    />
-  );
+const ListOfGifs = (props) => {
+  const { keyword } = props.params;
+  // const [loading, setLoading] = useState(false);
+  // const [gifs, setGifs] = useState([]);
+  const [gifs, setGifs] = useState({
+    loading: false,
+    results: []
+  });
+
+  useEffect(() => {
+    // setLoading(true);
+    setGifs(actualGifs => ({
+      loading: true,
+      results: actualGifs.results
+    }));
+
+    getGifs({ keyword })
+      .then(gifs => {
+        // setGifs(gifs);
+        // setLoading(false);
+        setGifs({
+          loading: false,
+          results: gifs
+        });
+    });
+  }, [keyword]);
+
+  if (gifs.loading) return <i>Cargando ⏳</i>
+
+  return <>
+    {
+      gifs.results.map(({ id, title, url }) =>
+        <Gif
+          key={id}
+          id={id}
+          title={title}
+          url={url}
+        />
+      )
+    }
+  </>
 };
 
 export default ListOfGifs;
