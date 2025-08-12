3be09df941c4173e6a4586a2d93f7c5227c0ce59
Franco <francocorniglione5@gmail.com>
2025-08-11 23:25:59 -0300

    modifying App.js first approach

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import getGifs from "./services/getGifs";
+import ListOfGifs from "./components/ListOfGifs";
 
-function App() {
+const App = () => {
+  const [gifs, setGifs] = useState([]);
+
+  useEffect(() => {
+    getGifs({ keyword: "programming" }).then(gifs => setGifs(gifs));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="App-content">
+        <ListOfGifs gifs={gifs} />
+      </section>
     </div>
   );
-}
+};
 
 export default App;
