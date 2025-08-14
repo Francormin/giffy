35bd65069ba8774e14ce9af5c18535a04a102879
Franco <francocorniglione5@gmail.com>
2025-08-14 19:16:50 -0300

    minor changes to getGifsByKeyword service

--- src/services/getGifsByKeyword.js
+++ src/services/getGifsByKeyword.js
@@ -1,5 +1,5 @@
-const getGifsByKeyword = ({ keyword = "morty" } = {}) => {
-  const params = `search?api_key=${process.env.REACT_APP_GIPHY_API_KEY}&q=${keyword}&limit=10&offset=0&rating=g&lang=en&bundle=messaging_non_clips`;
+const getGifsByKeyword = ({ keyword } = {}) => {
+  const params = `search?api_key=${process.env.REACT_APP_GIPHY_API_KEY}&q=${keyword}&limit=12&offset=0&rating=g&lang=en&bundle=messaging_non_clips`;
   const apiUrl = `${process.env.REACT_APP_GIPHY_BASE_URL}${params}`;
 
   return fetch(apiUrl)
