007b7109369ebebd3d31736f2bfdbe543e6eb6b9
Franco <francocorniglione5@gmail.com>
2025-08-15 12:35:19 -0300

    adding useGifs new custom hook

---
+++ src/hooks/useGifs.js
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import getGifsByKeyword from "../services/getGifsByKeyword";
+
+const useGifs = ({ keyword } = {}) => {
+  // const [loading, setLoading] = useState(false);
+  // const [gifs, setGifs] = useState([]);
+  const [gifs, setGifs] = useState({
+    loading: false,
+    results: []
+  });
+
+  useEffect(() => {
+    // setLoading(true);
+    setGifs(actualGifs => ({
+      loading: true,
+      results: actualGifs.results
+    }));
+
+    const keywordToUse = keyword || localStorage.getItem("lastKeyword") || "random";
+
+    getGifsByKeyword({ keyword: keywordToUse }).then(gifs => {
+      // setGifs(gifs);
+      // setLoading(false);
+      setGifs({
+        loading: false,
+        results: gifs
+      });
+      localStorage.setItem("lastKeyword", keywordToUse)
+    });
+  }, [keyword]);
+
+  return gifs;
+};
+
+export default useGifs;
