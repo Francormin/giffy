bb7d7bdb5cbf001fbd1a5cebc57a9b9bca595cc9
Franco <francocorniglione5@gmail.com>
2025-08-15 12:40:41 -0300

    modularizing ListOfGifs component stuff

---
+++ src/components/ListOfGifs/index.js
@@ -0,0 +1,25 @@
+import Gif from "../Gif";
+import ResourceNotFound from "../ResourceNotFound";
+import Spinner from "../Spinner";
+import "./styles.css";
+
+const ListOfGifs = ({ gifs, loading }) => {
+  return loading ? (
+    <Spinner />
+  ) : gifs.length === 0 ? (
+    <ResourceNotFound />
+  ) : (
+    <div className="ListOfGifs">
+      {gifs.map(({ id, title, url }) =>
+        <Gif
+          key={id}
+          id={id}
+          title={title}
+          url={url}
+        />
+      )}
+    </div>
+  )
+};
+
+export default ListOfGifs;
