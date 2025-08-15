9d6ed675a59d98a176c2acb38975d192136f6a9d
Franco <francocorniglione5@gmail.com>
2025-08-15 12:39:21 -0300

    modularizing Gif component stuff

---
+++ src/components/Gif/index.js
@@ -0,0 +1,15 @@
+import { Link } from "wouter";
+import "./styles.css";
+
+const Gif = ({ id, title, url }) => {
+  return (
+    <div className="Gif-card">
+      <Link href={`/gif/${id}`} className="Gif-link">
+        <h4>{title}</h4>
+        <img src={url} alt={title} loading="lazy" />
+      </Link>
+    </div>
+  );
+};
+
+export default Gif;
