22ddb2a840ab7b94038851d3b68fe81142466529
Franco <francocorniglione5@gmail.com>
2025-08-15 12:37:24 -0300

    adding new ResourceNotFound component

---
+++ src/components/ResourceNotFound/index.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+const ResourceNotFound = () => {
+  return (
+    <div className="ResourceNotFound-container">
+      <img src="/error_404.png" alt="Resource Not Found" />
+    </div>
+  );
+};
+
+export default ResourceNotFound;
