1c7de1a41dad200f7ceee841d98c8b6a0091e7e3
Franco <francocorniglione5@gmail.com>
2025-08-18 16:48:38 -0300

    setting and wrapping wouter routes with react context provider

--- src/App.js
+++ src/App.js
@@ -3,23 +3,26 @@ import Home from "./pages/Home";
 import SearchResults from "./pages/SearchResults";
 import Detail from "./pages/Detail";
 import NotFound from "./pages/NotFound";
+import { GifsContextProvider } from "./context/GifsContext";
 import "./App.css";
 
 const App = () => {
   return (
     <div className="App">
-      <section className="App-content">
-        <Link to="/" className="App-logo-link">
-          <img className="App-logo pulse" src="/giffy_logo.png" alt="giffy logo" />
-        </Link>
+    <section className="App-content">
+      <Link to="/" className="App-logo-link">
+        <img className="App-logo pulse" src="/giffy_logo.png" alt="giffy logo" />
+      </Link>
+      <GifsContextProvider>
         <Switch>
           <Route path="/" component={Home} />
           <Route path="/search/:keyword" component={SearchResults} />
           <Route path="/gif/:id" component={Detail} />
           <Route component={NotFound} />
         </Switch>
-      </section>
-    </div>
+      </GifsContextProvider>
+    </section>
+  </div>
   );
 };
 
