da5580813c6081a819f1c3b70ea66073fb972cb9
Franco <francocorniglione5@gmail.com>
2025-08-18 16:57:00 -0300

    refactoring GifDetails component

--- src/components/GifDetails/index.js
+++ src/components/GifDetails/index.js
@@ -1,19 +1,24 @@
 import ResourceNotFound from "../ResourceNotFound";
+import Spinner from "../Spinner";
 import "./styles.css";
 
-const GifDetails = ({ gif }) => {
+const GifDetails = ({ gif, loading }) => {
   const hasData = gif && Object.keys(gif).length > 0;
 
-  return !hasData ? (
+  return loading ? (
+    <Spinner />
+  ) : !hasData ? (
     <ResourceNotFound />
   ) : (
     <div className="GifDetails-container">
-      <img src={gif.url} alt="url" />
-      <h3>Title: {gif.title}</h3>
-      <span>Type: {gif.type}</span>
-      <span>Description: {gif.description || "no description specified"}</span>
-      <span>Rating: {gif.rating}</span>
-      <span>Username: {gif.username || "no username specified"}</span>
+      <div className="GifDetails-card">
+        <img src={gif.url} alt="url" />
+        <h3>Title: {gif.title}</h3>
+        <span>Type: {gif.type}</span>
+        <span>Description: {gif.description || "no description specified"}</span>
+        <span>Rating: {gif.rating}</span>
+        <span>Username: {gif.username || "no username specified"}</span>
+      </div>
     </div>
   );
 };
