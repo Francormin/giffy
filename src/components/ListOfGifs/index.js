fd8934b85e1c2717af840ea18a3ece9a79e049d5
Franco <francocorniglione5@gmail.com>
2025-08-19 19:12:53 -0300

    ListOfGifs component minor changes

--- src/components/ListOfGifs/index.js
+++ src/components/ListOfGifs/index.js
@@ -3,21 +3,24 @@ import ResourceNotFound from "../ResourceNotFound";
 import Spinner from "../Spinner";
 import "./styles.css";
 
-const ListOfGifs = ({ gifs, loading }) => {
+const ListOfGifs = ({ gifs, loading, keyword }) => {
   return loading ? (
     <Spinner />
   ) : gifs?.length === 0 ? (
     <ResourceNotFound />
   ) : (
-    <div className="ListOfGifs">
-      {gifs?.map(({ id, title, url }) =>
-        <Gif
-          key={id}
-          id={id}
-          title={title}
-          url={url}
-        />
-      )}
+    <div className="ListOfGifs-container">
+      <h5><u>results for:</u> {keyword}</h5>
+      <div className="ListOfGifs-grid">
+        {gifs?.map(({ id, title, url }) =>
+          <Gif
+            key={id}
+            id={id}
+            title={title}
+            url={url}
+          />
+        )}
+      </div>
     </div>
   )
 };
