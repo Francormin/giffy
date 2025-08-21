2af787b5159aad37141c070370f1df58c502b5e0
Franco <francocorniglione5@gmail.com>
2025-08-21 13:30:55 -0300

    updating App with ScrollToTop component to render in all routes

--- src/App.js
+++ src/App.js
@@ -1,29 +1,31 @@
 import { Link, Route, Switch } from "wouter";
+import { GifsContextProvider } from "context/GifsContext";
+import ScrollToTop from "components/ScrollToTop";
 import Home from "pages/Home";
 import SearchResults from "pages/SearchResults";
 import Detail from "pages/Detail";
 import NotFound from "pages/NotFound";
-import { GifsContextProvider } from "context/GifsContext";
 import "App.css";
 import "theme.css";
 
 const App = () => {
   return (
     <div className="App">
-    <section className="App-content">
-      <Link to="/" className="App-logo-link">
-        <img className="App-logo pulse" src="/giffy_logo.png" alt="giffy logo" />
-      </Link>
-      <GifsContextProvider>
-        <Switch>
-          <Route path="/" component={Home} />
-          <Route path="/search/:keyword" component={SearchResults} />
-          <Route path="/gif/:id" component={Detail} />
-          <Route component={NotFound} />
-        </Switch>
-      </GifsContextProvider>
-    </section>
-  </div>
+      <section className="App-content">
+        <Link to="/" className="App-logo-link">
+          <img className="App-logo pulse" src="/giffy_logo.png" alt="giffy logo" />
+        </Link>
+        <GifsContextProvider>
+          <ScrollToTop />
+          <Switch>
+            <Route path="/" component={Home} />
+            <Route path="/search/:keyword" component={SearchResults} />
+            <Route path="/gif/:id" component={Detail} />
+            <Route component={NotFound} />
+          </Switch>
+        </GifsContextProvider>
+      </section>
+    </div>
   );
 };
 
