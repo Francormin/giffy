a8584bb9bad720c75cbdffbf6bdb233264ed1704
Franco <francocorniglione5@gmail.com>
2025-08-22 18:56:44 -0300

    updating SearchResults page to handle new pagination feature

--- src/pages/SearchResults/index.js
+++ src/pages/SearchResults/index.js
@@ -3,7 +3,9 @@ import ListOfGifs from "components/ListOfGifs";
 
 const SearchResults = ({ params }) => {
   const { keyword } = params;
-  const { loading, results } = useGifs({ keyword });
+  const { loading, results, setPage } = useGifs({ keyword });
+
+  const handleNextPage = () => setPage(prevPage => prevPage + 1);
 
   return (
     <div className="SearchResults-container">
@@ -11,6 +13,9 @@ const SearchResults = ({ params }) => {
         <u>results for:</u> {keyword}
       </h5>
       <ListOfGifs gifs={results} loading={loading} />
+      <button onClick={handleNextPage}>
+        Get next page
+      </button>
     </div>
   );
 };
