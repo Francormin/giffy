72010e316da18beb09c9b2bd150afc50c40cf6ab
Franco <francocorniglione5@gmail.com>
2025-09-19 10:32:46 -0300

    adding some config in order to get the tests running

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,48 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// Mock para window.scrollTo
+window.scrollTo = jest.fn();
+
+// Mock para IntersectionObserver
+class IntersectionObserver {
+  constructor(callback, options) {}
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+}
+Object.defineProperty(window, 'IntersectionObserver', {
+  writable: true,
+  configurable: true,
+  value: IntersectionObserver,
+});
+Object.defineProperty(global, 'IntersectionObserver', {
+  writable: true,
+  configurable: true,
+  value: IntersectionObserver,
+});
+
+beforeEach(() => {
+  jest.spyOn(global, "fetch").mockResolvedValue({
+    json: () =>
+      Promise.resolve({
+        data: [
+          {
+            id: "abc123",
+            title: "Funny cat",
+            images: { original: { url: "http://fake-cat.gif" } },
+          },
+          {
+            id: "xyz456",
+            title: "Dancing dog",
+            images: { original: { url: "http://fake-dog.gif" } },
+          },
+        ],
+      }),
+  });
+});
+
+afterEach(() => {
+  jest.restoreAllMocks();
+});
