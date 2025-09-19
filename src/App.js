ee7a1c912aaab76f304bd005505e489fb59303ec
Franco <francocorniglione5@gmail.com>
2025-09-19 10:36:11 -0300

    making Home page to load in lazy mode

--- src/App.js
+++ src/App.js
@@ -1,30 +1,34 @@
+import React, { Suspense } from "react";
 import { Link, Route, Switch } from "wouter";
 import { GifsContextProvider } from "context/GifsContext";
 import ScrollToTop from "components/ScrollToTop";
-import Home from "pages/Home";
 import SearchResults from "pages/SearchResults";
 import Detail from "pages/Detail";
 import NotFound from "pages/NotFound";
 import "App.css";
 import "theme.css";
 
+const HomePage = React.lazy(() => import("pages/Home"));
+
 const App = () => {
   return (
     <div className="App">
-      <section className="App-content">
-        <Link to="/" className="App-logo-link">
-          <img className="App-logo pulse" src="/giffy_logo.png" alt="giffy logo" />
-        </Link>
-        <GifsContextProvider>
-          <ScrollToTop />
-          <Switch>
-            <Route path="/" component={Home} />
-            <Route path="/search/:keyword" component={SearchResults} />
-            <Route path="/gif/:id" component={Detail} />
-            <Route component={NotFound} />
-          </Switch>
-        </GifsContextProvider>
-      </section>
+      <Suspense fallback={null}>
+        <section className="App-content">
+          <Link to="/" className="App-logo-link">
+            <img className="App-logo pulse" src="/giffy_logo.png" alt="giffy logo" />
+          </Link>
+          <GifsContextProvider>
+            <ScrollToTop />
+            <Switch>
+              <Route path="/" component={HomePage} />
+              <Route path="/search/:keyword" component={SearchResults} />
+              <Route path="/gif/:id" component={Detail} />
+              <Route component={NotFound} />
+            </Switch>
+          </GifsContextProvider>
+        </section>
+      </Suspense>
     </div>
   );
 };
