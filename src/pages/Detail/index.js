74ce78a1243fbedccb79ad64a2715c46e8d25445
Franco <francocorniglione5@gmail.com>
2025-10-01 15:11:44 -0300

    fixing Detail page to show correct gif details when page is reloaded

--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -1,12 +1,10 @@
-import useGlobalGifs from "hooks/useGlobalGifs";
 import GifDetails from "components/GifDetails";
 import Spinner from "components/Spinner";
+import useSingleGif from "hooks/useSingleGif";
 
 const Detail = ({ params }) => {
   const { id } = params;
-  const { loading, results } = useGlobalGifs();
-
-  const gif = results?.find(singleGif => singleGif.id === id);
+  const { loading, gif } = useSingleGif(id);
 
   return loading ? <Spinner /> : <GifDetails gif={gif} />;
 };
