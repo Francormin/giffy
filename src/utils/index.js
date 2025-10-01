a0d76debc1afe6de0dd3506675fab8d06cf7936b
Franco <francocorniglione5@gmail.com>
2025-10-01 15:10:39 -0300

    updating fromApiResponseToGifs auxiliar function

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,22 +1,39 @@
 export const fromApiResponseToGifs = apiResponse => {
-  const { data = [] } = apiResponse;
+  const { data } = apiResponse;
 
-  if (data.length === 0) return data;
+  if (Array.isArray(data)) {
+    if (data.length === 0) return data;
 
-  const gifs = data.map(gif => {
-    const { id, type, title, alt_text, rating, username, images } = gif;
-    const { url } = images.original;
+    const gifs = data.map(gif => {
+      const { id, type, title, alt_text, rating, username, images } = gif;
+      const { url } = images.original;
 
-    return {
-      id,
-      type,
-      title,
-      description: alt_text,
-      rating,
-      username,
-      url
-    };
-  });
+      return {
+        id,
+        type,
+        title,
+        description: alt_text,
+        rating,
+        username,
+        url
+      };
+    });
 
-  return gifs;
+    return gifs;
+  }
+
+  if (Object.keys(data).length === 0) return data;
+
+  const { id, type, title, alt_text, rating, username, images } = data;
+  const { url } = images.original;
+
+  return {
+    id,
+    type,
+    title,
+    description: alt_text,
+    rating,
+    username,
+    url
+  };
 };
