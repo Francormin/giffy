d3bd71f5fb8f3a872e1e6b26b91649281750d593
Franco <francocorniglione5@gmail.com>
2025-10-14 23:18:32 -0300

    making some minor changes to ListOfGifs component

--- src/components/ListOfGifs/index.js
+++ src/components/ListOfGifs/index.js
@@ -1,25 +1,20 @@
 import Gif from "components/Gif";
-import ResourceNotFound from "components/ResourceNotFound";
 import "./styles.css";
 
-const ListOfGifs = ({ gifs }) => {
-  return gifs?.length === 0 ? (
-    <ResourceNotFound />
-  ) : (
-    <div className="ListOfGifs-container">
-      <div className="ListOfGifs-grid">
-        {gifs?.map(({ id, title, url, ...restOfGif }) =>
-          <Gif
-            key={id}
-            id={id}
-            title={title}
-            url={url}
-            extraInfo={restOfGif}
-          />
-        )}
-      </div>
+const ListOfGifs = ({ gifs }) => (
+  <div className="ListOfGifs-container">
+    <div className="ListOfGifs-grid">
+      {gifs?.map(({ id, title, url, ...restOfGif }) => (
+        <Gif
+          key={id}
+          id={id}
+          title={title}
+          url={url}
+          extraInfo={restOfGif}
+        />
+      ))}
     </div>
-  );
-};
+  </div>
+);
 
 export default ListOfGifs;
