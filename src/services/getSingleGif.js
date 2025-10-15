b50d2bd6cc80e295809834d85b97f9dd1aacba15
Franco <francocorniglione5@gmail.com>
2025-10-15 18:05:50 -0300

    updating logic in getSingleGif service

--- src/services/getSingleGif.js
+++ src/services/getSingleGif.js
@@ -1,13 +1,22 @@
 import { fromApiResponseToGifs } from "utils";
+import CustomError from "utils/CustomError";
 
 const getSingleGif = id => {
   const params = `/gifs/${id}?api_key=${process.env.REACT_APP_GIPHY_API_KEY}`;
   const apiUrl = `${process.env.REACT_APP_GIPHY_BASE_URL}${params}`;
 
   return fetch(apiUrl)
-    .then(res => res.json())
+    .then(res => {
+      if (!res.ok) throw res.status;
+      return res.json();
+    })
     .then(fromApiResponseToGifs)
-    .catch(error => console.error("Error fetching single gif:", error));
-}
+    .catch(status => {
+      const baseError = "Error fetching single gif: ";
+      throw status === 404
+        ? new CustomError(baseError + "Resource not found", status)
+        : new CustomError(baseError + "Generic error", status);
+    });
+};
 
 export default getSingleGif;
