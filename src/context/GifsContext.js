aa000d03ba62130b8d119279f65047cb8b88942b
Franco <francocorniglione5@gmail.com>
2025-10-15 18:02:45 -0300

    syncing React context with data stored in localStorage

--- src/context/GifsContext.js
+++ src/context/GifsContext.js
@@ -1,9 +1,18 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
-const GifsContext = createContext({});
+const GifsContext = createContext();
 
 export const GifsContextProvider = ({ children }) => {
-  const [gifs, setGifs] = useState({});
+  const [gifs, setGifs] = useState(() => {
+    // Inicializa desde localStorage
+    const localData = localStorage.getItem("gifs");
+    return localData ? JSON.parse(localData) : [];
+  });
+
+  useEffect(() => {
+    // Actualiza localStorage cada vez que cambian los gifs
+    localStorage.setItem("gifs", JSON.stringify(gifs));
+  }, [gifs]);
 
   return (
     <GifsContext.Provider value={{gifs, setGifs}}>
