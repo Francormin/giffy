1e63dc906a39869d0ebaeec7cf84aeeb9f8c3809
Franco <francocorniglione5@gmail.com>
2025-10-14 23:13:41 -0300

    refactoring utils index

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,39 +1,23 @@
+const mapGif = ({ id, type, title, alt_text, rating, username, images }) => ({
+  id,
+  type,
+  title,
+  description: alt_text,
+  rating,
+  username,
+  url: images?.original?.url
+});
+
 export const fromApiResponseToGifs = apiResponse => {
+  if ("data" in apiResponse === false) return [];
+  
   const { data } = apiResponse;
 
   if (Array.isArray(data)) {
     if (data.length === 0) return data;
-
-    const gifs = data.map(gif => {
-      const { id, type, title, alt_text, rating, username, images } = gif;
-      const { url } = images.original;
-
-      return {
-        id,
-        type,
-        title,
-        description: alt_text,
-        rating,
-        username,
-        url
-      };
-    });
-
-    return gifs;
+    return data.map(mapGif);
   }
 
   if (Object.keys(data).length === 0) return data;
-
-  const { id, type, title, alt_text, rating, username, images } = data;
-  const { url } = images.original;
-
-  return {
-    id,
-    type,
-    title,
-    description: alt_text,
-    rating,
-    username,
-    url
-  };
+  return mapGif(data);
 };
