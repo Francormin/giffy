b28ef331df9a77067042ea7098501caed5a3d170
Franco <francocorniglione5@gmail.com>
2025-12-16 20:37:29 -0300

    css styling stuff

--- src/components/Gif/index.js
+++ src/components/Gif/index.js
@@ -5,7 +5,7 @@ import "./styles.css";
 const Gif = ({ id, title, url }) => {
   return (
     <div className="Gif-card">
-      <Link href={`/gif/${id}`} className="Gif-link">
+      <Link to={`/gif/${id}`} className="Gif-link">
         <h4>{title}</h4>
         <img src={url} alt={title} loading="lazy" />
       </Link>
--- src/components/ListOfGifs/index.js
+++ src/components/ListOfGifs/index.js
@@ -2,18 +2,16 @@ import Gif from "components/Gif";
 import "./styles.css";
 
 const ListOfGifs = ({ gifs }) => (
-  <div className="ListOfGifs-container">
-    <div className="ListOfGifs-grid">
-      {gifs?.map(({ id, title, url, ...restOfGif }) => (
-        <Gif
-          key={id}
-          id={id}
-          title={title}
-          url={url}
-          extraInfo={restOfGif}
-        />
-      ))}
-    </div>
+  <div className="ListOfGifs-grid">
+    {gifs?.map(({ id, title, url, ...restOfGif }) => (
+      <Gif
+        key={id}
+        id={id}
+        title={title}
+        url={url}
+        extraInfo={restOfGif}
+      />
+    ))}
   </div>
 );
 
