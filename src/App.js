fa45fc7e6290e9dcc80110bb7bc631f941b7851d
Franco <francocorniglione5@gmail.com>
2025-12-22 19:01:20 -0300

    react lazy and react suspense good practices

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import SearchResults from "pages/SearchResults";
 import Detail from "pages/Detail";
 import NotFound from "pages/NotFound";
 import ScrollToTop from "components/ScrollToTop";
+import Spinner from "components/Spinner";
 import "App.css";
 import "theme.css";
 
@@ -13,23 +14,25 @@ const HomePage = React.lazy(() => import("pages/Home"));
 const App = () => {
   return (
     <div className="App">
-      <Suspense fallback={null}>
-        <section className="App-content">
-          <Link to="/" className="App-logo-link">
-            <img className="App-logo pulse" src="/giffy_logo.png" alt="giffy logo" />
-          </Link>
-          <GifsContextProvider>
-            <ScrollToTop />
-            <Switch>
-              <Route path="/" component={HomePage} />
-              <Route path="/search/:keyword/:rating?" component={SearchResults} />
-              <Route path="/gif/:id" component={Detail} />
-              <Route path="/404" component={NotFound} />
-              <Route component={NotFound} />
-            </Switch>
-          </GifsContextProvider>
-        </section>
-      </Suspense>
+      <section className="App-content">
+        <Link to="/" className="App-logo-link">
+          <img className="App-logo pulse" src="/giffy_logo.png" alt="giffy logo" />
+        </Link>
+        <GifsContextProvider>
+          <ScrollToTop />
+          <Switch>
+            <Route path="/">
+              <Suspense fallback={<Spinner />}>
+                <HomePage />
+              </Suspense>
+            </Route>
+            <Route path="/search/:keyword/:rating?" component={SearchResults} />
+            <Route path="/gif/:id" component={Detail} />
+            <Route path="/404" component={NotFound} />
+            <Route component={NotFound} />
+          </Switch>
+        </GifsContextProvider>
+      </section>
     </div>
   );
 };
