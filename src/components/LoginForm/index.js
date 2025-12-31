359ab36b00b6815a1a2fa8f96e311b49722d1382
Franco <francocorniglione5@gmail.com>
2025-12-31 16:51:08 -0300

    separating logic between Login page and component and adding some css styles to LoginForm

---
+++ src/components/LoginForm/index.js
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import "./styles.css";
+
+const LoginForm = ({ login, loading, error }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    login({ username, password });
+  };
+
+  return (
+    <div className="login-container">
+      {loading && <p className="login-loading">Checking credentials...</p>}
+
+      {!loading && (
+        <form className="login-form" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={username}
+            onChange={e => setUsername(e.target.value)}
+            placeholder="Username"
+          />
+          <input
+            type="password"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+            placeholder="Password"
+          />
+          <button type="submit" disabled={!username || !password}>
+            Login
+          </button>
+        </form>
+      )}
+
+      {error && <p className="login-error">Credentials are invalid!</p>}
+    </div>
+  );
+};
+
+export default LoginForm;
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,48 +1,22 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useLocation } from "wouter";
 import useUser from "hooks/useUser";
+import LoginForm from "components/LoginForm";
 
 const Login = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-
   const [, navigate] = useLocation();
-
   const { isLogged, login, loginIsLoading, loginHasError } = useUser();
-
-  const handleSubmit = e => {
-    e.preventDefault();
-    login({ username, password });
-  };
 
   useEffect(() => {
     if (isLogged) navigate("/");
   }, [isLogged, navigate]);
 
   return (
-    <>
-      {loginIsLoading && <strong>Checking credentials...</strong>}
-
-      {!loginIsLoading && (
-        <form onSubmit={handleSubmit}>
-          <input
-            type="text"
-            value={username}
-            onChange={e => setUsername(e.target.value)}
-            placeholder="Username"
-          />
-          <input
-            type="password"
-            value={password}
-            onChange={e => setPassword(e.target.value)}
-            placeholder="Password"
-          />
-          <button type="submit">Login</button>
-        </form>
-      )}
-
-      {loginHasError && <strong>Credentials are invalid!</strong>}
-    </>
+    <LoginForm
+      login={login}
+      loading={loginIsLoading}
+      error={loginHasError}
+    />
   );
 };
 
