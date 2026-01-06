cac0719e9d7fa411ddc06e7da90174b8ab68db6c
Franco <francocorniglione5@gmail.com>
2026-01-06 19:32:42 -0300

    refactoring SearchResults page with useInfiniteScroll new custom hook

--- src/pages/SearchResults/index.js
+++ src/pages/SearchResults/index.js
@@ -1,9 +1,7 @@
-import { useEffect, useRef } from "react";
 import { Helmet } from "react-helmet";
 import { Redirect } from "wouter";
-import debounce from "just-debounce-it";
 import useGifs from "hooks/useGifs";
-import useNearScreen from "hooks/useNearScreen";
+import useInfiniteScroll from "hooks/useInfiniteScroll";
 import ListOfGifs from "components/ListOfGifs";
 import Spinner from "components/Spinner";
 import SearchForm from "components/SearchForm";
@@ -11,31 +9,20 @@ import SearchForm from "components/SearchForm";
 const SearchResults = ({ params }) => {
   const { keyword, rating = "g", language = "en" } = params;
 
-  const { gifs, isLoading, isResultEmpty, page, setPage } = useGifs({
+  const { gifs, isLoading, isResultEmpty, setPage } = useGifs({
     keyword,
     rating,
     language
   });
 
-  const debounceHandleNextPage = useRef(
-    debounce(() => {
-      setPage(prev => prev + 1)
-    }, 500)
-  ).current;
-
-  const externalRef = useRef();
-
-  const { show } = useNearScreen({
-    externalRef: isLoading ? null : externalRef,
-    once: false
+  const { externalRef } = useInfiniteScroll({
+    isLoading,
+    onLoadMore: () => setPage(prev => prev + 1)
   });
 
-  useEffect(() => {
-    if (!show || isLoading) return;
-    debounceHandleNextPage();
-  }, [show, isLoading, page, debounceHandleNextPage]);
+  if (isResultEmpty) return <Redirect to="/404" />;
 
-  return !isResultEmpty ? (
+  return (
     <>
       <Helmet>
         <title>{`Search results for ${decodeURIComponent(keyword)} | Giffy`}</title>
@@ -61,8 +48,6 @@ const SearchResults = ({ params }) => {
         {isLoading && <Spinner />}
       </div>
     </>
-  ) : (
-    <Redirect to="/404" />
   );
 };
 
