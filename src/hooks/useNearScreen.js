065feb2c9c340491e5cff5ce5086738cbe1dbf2b
Franco <francocorniglione5@gmail.com>
2026-01-06 19:31:34 -0300

    refactoring useNearScreen custom hook

--- src/hooks/useNearScreen.js
+++ src/hooks/useNearScreen.js
@@ -1,13 +1,11 @@
 import { useEffect, useRef, useState } from "react";
 
-const useNearScreen = ({ distance = "100px", externalRef, once = true } = {}) => {
+const useNearScreen = ({ distance = "100px", threshold = 0, externalRef, once = true } = {}) => {
   const [show, setShow] = useState(false);
   const elementRef = useRef();
 
   useEffect(() => {
-    const element = externalRef
-      ? externalRef.current
-      : elementRef.current;
+    const element = externalRef ? externalRef.current : elementRef.current;
 
     const onChange = (entries, observer) => {
       const isIntersecting = entries[0].isIntersecting;
@@ -21,13 +19,14 @@ const useNearScreen = ({ distance = "100px", externalRef, once = true } = {}) =>
     };
 
     const observer = new IntersectionObserver(onChange, {
-      rootMargin: distance
+      rootMargin: distance,
+      threshold
     });
-    
+
     element && observer.observe(element);
 
     return () => observer.disconnect();
-  });
+  }, [externalRef, distance, threshold, once]);
 
   return { show, elementRef };
 };
