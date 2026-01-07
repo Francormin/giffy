62a9bb1fba19547ef90f2241c698aaf9dda7f789
Franco <francocorniglione5@gmail.com>
2026-01-07 18:03:32 -0300

    updating UserContextProvider with new favs state

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,20 +1,28 @@
 import { createContext, useEffect, useState } from "react";
+import getFavs from "services/getFavs";
 
 const UserContext = createContext();
 
 export const UserContextProvider = ({ children }) => {
+  const [favs, setFavs] = useState([]);
+
   const [jwt, setJwt] = useState(() => {
     const localData = sessionStorage.getItem("jwt");
     return localData ? JSON.parse(localData) : null;
   });
 
   useEffect(() => {
-    if (jwt) sessionStorage.setItem("jwt", JSON.stringify(jwt));
-    else sessionStorage.removeItem("jwt");
+    if (jwt) {
+      sessionStorage.setItem("jwt", JSON.stringify(jwt));
+      getFavs({ jwt }).then(setFavs);
+    } else {
+      sessionStorage.removeItem("jwt");
+      setFavs([]);
+    }
   }, [jwt]);
 
   return (
-    <UserContext.Provider value={{ jwt, setJwt }}>
+    <UserContext.Provider value={{ favs, jwt, setFavs, setJwt }}>
       {children}
     </UserContext.Provider>
   );
