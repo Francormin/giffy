d846c582f03b030819809f6d12b4fe4ca6918640
Franco <francocorniglione5@gmail.com>
2026-01-13 22:20:35 -0300

    adding minor logic in the render of Header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,15 +1,21 @@
-import { Link } from "wouter";
+import { Link, useRoute } from "wouter";
 import useUser from "hooks/useUser";
 import "./styles.css";
 
 const Header = () => {
+  const [match] = useRoute("/login");
   const { isLogged, logout } = useUser();
+
+  const renderLogButtons = () =>
+    isLogged
+      ? <button onClick={logout}>Logout</button>
+      : <Link to="/login">Login</Link>;
+
+  const content = match ? null : renderLogButtons();
 
   return (
     <div className="Header-component">
-      {isLogged
-        ? <button onClick={logout}>Logout</button>
-        : <Link to="/login">Login</Link>}
+      {content}
     </div>
   );
 };
