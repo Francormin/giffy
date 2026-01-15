e30f50de8dbef43837e5f355dabb0ad295e529da
Franco <francocorniglione5@gmail.com>
2026-01-15 15:33:03 -0300

    redirecting to login page when user logout

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,14 +1,20 @@
-import { Link, useRoute } from "wouter";
+import { Link, useRoute, useLocation } from "wouter";
 import useUser from "hooks/useUser";
 import "./styles.css";
 
 const Header = () => {
+  const [, navigate] = useLocation();
   const [match] = useRoute("/login");
   const { isLogged, logout } = useUser();
 
+  const handleLogout = () => {
+    logout();
+    navigate("/login");
+  };
+
   const renderLogButtons = () =>
     isLogged
-      ? <button onClick={logout}>Logout</button>
+      ? <button onClick={handleLogout}>Logout</button>
       : <Link to="/login">Login</Link>;
 
   const content = match ? null : renderLogButtons();
