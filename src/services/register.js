47bfbb5c749b7b1ddc4373eb956d904b363fa5a1
Franco <francocorniglione5@gmail.com>
2026-01-17 15:07:05 -0300

    making register service an async await fn

--- src/services/register.js
+++ src/services/register.js
@@ -1,14 +1,13 @@
-const register = ({ username, password }) => {
-  return fetch(`${process.env.REACT_APP_API_ENDPOINT}/register`, {
+const register = async ({ username, password }) => {
+  const response = await fetch(`${process.env.REACT_APP_API_ENDPOINT}/register`, {
     method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
+    headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ username, password })
-  }).then(response => {
-    if (response.status === 409) throw new Error("User already exists");
-    return true;
   });
+
+  if (response.status === 409) throw new Error("User already exists");
+  if (!response.ok) throw new Error("Register failed");
+  return true;
 };
 
 export default register;
