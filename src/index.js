fc64c94d767653dc7a5b79e799b1db81fa3955b7
Franco <francocorniglione5@gmail.com>
2026-01-27 20:38:37 -0300

    making use of ThemeProvider component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
+import { ThemeProvider } from "@emotion/react";
+import { theme } from "styles";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "./index.css";
@@ -9,7 +11,9 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <HelmetProvider>
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </HelmetProvider>
   </React.StrictMode>
 );
