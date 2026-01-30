ae4fb0cfe65143fe9d53e833840a4681fc3055ca
Franco <francocorniglione5@gmail.com>
2026-01-30 17:23:24 -0300

    adding new service to remove gif from user favs

---
+++ src/services/removeFav.js
@@ -0,0 +1,15 @@
+const removeFav = async ({ id, jwt }) => {
+  const response = await fetch(`${process.env.REACT_APP_API_ENDPOINT}/favs/${id}`, {
+    method: "DELETE",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ jwt })
+  });
+
+  if (!response.ok) throw new Error();
+
+  const parsedResponse = await response.json();
+  const { favs } = parsedResponse;
+  return favs;
+};
+
+export default removeFav;
